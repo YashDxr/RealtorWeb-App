83bf9921feaa00da7cfe9bef403765e7c92f7def
YashDxr <yashdxr@gmail.com>
2023-10-15 17:57:41 +0530

    created test api route using Express router and created routes,controller to distribute logic according to industry practice

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from './routes/user.route.js';
 import dotenv from "dotenv"
 dotenv.config();
 
@@ -22,3 +23,10 @@ const app = express();
 app.listen(3000, () => {
     console.log(`Server running on port 3000`);
 })
+
+
+
+
+
+//API Routes
+app.use("/api/user", userRouter);
