cc29298821bff4ba0bcd85748982937a020cc21f
YashDxr <yashdxr@gmail.com>
2023-10-15 16:23:17 +0530

    Connected to database , installed mongoose, dotenv

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
 
+
+
+//Database
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to database");
+}).catch((err) => {
+    console.log(err);
+});
+
+
+
+
+
+//Express
 const app = express();
 
 app.listen(3000, () => {
