b7d94fdb6b6754ef5c5db16754b1ecf5226e4b21
YashDxr <yashdxr@gmail.com>
2023-10-16 15:45:18 +0530

    RTK (Reduc Toolkit) Used and replaced conventional data drilling method. Created store and slice for RTK

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {ToastContainer , toast} from 'react-toastify'
 import"react-toastify/dist/ReactToastify.css"
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
+
+
 
 export default function SignIn() {
   const [formData, setformData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const {loading , error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
 
 
@@ -35,7 +41,8 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -46,16 +53,19 @@ export default function SignIn() {
       const data = await res.json();
       if (data.success === false) {
         toast.error(data.message, toastOptions);
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
+        // setError(data.message);
+        // setLoading(false);
         return;
       }
-      setLoading(false);
-      setError(null);
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      // setLoading(false);
+      // setError(error.message);
+      dispatch(signInFailure(error.message));
       toast.error(error.message, toastOptions);
     }
   };
