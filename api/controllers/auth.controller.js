414e88fb4aeb48db1ec485e4e0af3756fda850e1
YashDxr <yashdxr@gmail.com>
2023-10-16 08:41:20 +0530

    Signin api route created, installed jsonwebtoken and used jwt to create token and cookie to save session of a user in browser if he login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { customErrors } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -15,6 +16,24 @@ export const signup = async (req, res, next) => {
   } catch (err) {
     // res.status(499).json(err.message);
     // next(customErrors(550,'Test Error'));
+    next(err);
+  }
+};
+
+
+
+export const signin = async (req,res,next) =>{
+  const {email,password} = req.body;
+  try{
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(customErrors(404,"User Not Found"));
+    const validPassword = await bcryptjs.compare(password,validUser.password);
+    if(!validPassword) return next(customErrors(401,"Invalid Credentials"));
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET_KEY);
+    const { password: pass, ...REST} = validUser._doc;                                     // delete password from response before sending response to user for security purpose
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(REST);          //expires: 24*60*60
+
+  }catch(err){
     next(err);
   }
 };
