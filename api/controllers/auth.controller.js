b26e9e0a531bf05ff732734d2b71980c8d4f36cb
YashDxr <yashdxr@gmail.com>
2023-10-19 21:01:38 +0530

    created signout api route and logic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -62,3 +62,14 @@ export const google = async(req, res, next) => {
     
   }
 }
+
+
+
+export const signOut = async (req,res,next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User logged out successfully!');
+  } catch (error) {
+    next(error);
+  }
+}
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserStart,
   deleteUserFailure,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -92,16 +95,31 @@ export default function Profile() {
     try {
       dispatch(deleteUserStart());
       const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       const data = await res.json();
-      if( data === false ){
+      if (data === false) {
         dispatch(deleteUserFailure(data.message));
         return;
       }
       dispatch(deleteUserSuccess(data));
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess());
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
     }
   };
 
@@ -170,7 +188,10 @@ export default function Profile() {
         >
           Delete account
         </span>
-        <span className="text-red-700 cursor-pointer hover:underline">
+        <span
+          onClick={handleSignOut}
+          className="text-red-700 cursor-pointer hover:underline"
+        >
           Sign out
         </span>
       </div>
