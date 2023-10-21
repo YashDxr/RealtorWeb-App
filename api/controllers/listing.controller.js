3ef4bc04378942b33f8e4df9617bd8f5517e0ffb
YashDxr <yashdxr@gmail.com>
2023-10-21 12:51:19 +0530

    Delete listing functionality in Profile and delete listing route created in backend

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,12 +1,28 @@
 import Listing from "../models/listing.model.js";
+import { customErrors } from "../utils/error.js";
 
-export const createListing = async(req , res , next) => {
-    try {
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
-        const listing = await Listing.create(req.body);
-        return res.status(201).json(listing);
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
 
-    } catch (error) {
-        next(error);
-    }
-}
+  if (!listing) {
+    return next(customErrors(401, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(customErrors(401, "Unauthorized Access"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing deleted successfully");
+  } catch (error) {
+    next(error);
+  }
+};
