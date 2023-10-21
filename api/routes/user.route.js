bb78e80ab2036573c1d10c1ec26685e5e768b4c7
YashDxr <yashdxr@gmail.com>
2023-10-21 07:39:03 +0530

    created get listings route for user's self listings and logic provided in controller(user)

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { test, updateUser } from "../controllers/user.controller.js";
+import { getUserListings, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get('/test', test);
+router.get('/listings/:id', verifyToken ,getUserListings);
 router.post('/update/:id', verifyToken ,updateUser);
 router.delete('/delete/:id', verifyToken ,deleteUser);
 
