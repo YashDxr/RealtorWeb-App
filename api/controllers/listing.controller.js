a57ef071d34c086cfd2cb5cb9c439f3d7cfe64bc
YashDxr <yashdxr@gmail.com>
2023-10-22 09:35:31 +0530

    search api route created and logic for search using parameters defined

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -47,15 +47,64 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-
 export const getListing = async (req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
-    if(!listing){
-      return next(customErrors(404, 'Listing not found'));
+    if (!listing) {
+      return next(customErrors(404, "Listing not found"));
     }
     res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
-}
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9; //defines how many listings will be viewed on the page at the start
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    //If default behavior is used then search for any type of data
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    //filter used then according to query update the search
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      //regex: built in functionality for mongodb that lets us search for a term in the name anywhere - even if the specified term is in between the name
+      // $options: 'i' lets us search for word in any Case - uppercase or lowercase
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+      res.status(200).json(listings)
+  } catch (error) {
+    next(error);
+  }
+};
