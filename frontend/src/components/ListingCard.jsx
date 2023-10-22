1bf17502bab6d94adb9c983791193de5c5ebd182
YashDxr <yashdxr@gmail.com>
2023-10-22 23:53:52 +0530

    bout page completed and added favicon to the webapp

--- frontend/src/components/ListingCard.jsx
+++ frontend/src/components/ListingCard.jsx
@@ -21,55 +21,23 @@ export default function ListingCard(props) {
               {props.listing.address}
             </p>
           </div>
-          <p className="text-sm  text-gray-600 line-clamp-2">
+          <p className="text-sm text-gray-600 line-clamp-2">
             {props.listing.description}
           </p>
-          <p className="text-slate-500 mt-2 font-semibold flex text-center">
-            {props.listing.offer ? (
-              <div className="flex gap-9">
-                {props.listing.type === "rent" ? (
-                  <div className="flex flex-wrap">
-                    Discount Price:
-                    <div>
-                      &#8377;&nbsp;
-                      {props.listing.offerPrice.toLocaleString("hi")} /month
-                    </div>
-                  </div>
-                ) : (
-                  <div className="flex flex-wrap">
-                    Discount Price:
-                    <div>
-                      &#8377;&nbsp;
-                      {props.listing.offerPrice.toLocaleString("hi")}
-                    </div>
-                  </div>
-                )}
-
-                <div className="flex flex-wrap">
-                  Actual Price:&nbsp;
-                  <div className="line-through">
-                    (&#8377;&nbsp;{props.listing.price.toLocaleString("hi")})
-                  </div>
-                </div>
-              </div>
-            ) : (
-              //   <div className=" flex gap-0">
-              <div className="">
-                {props.listing.type === "rent"
-                  ? `${props.listing.price.toLocaleString("hi")}/month`
-                  : `${props.listing.price.toLocaleString("hi")}`}
-              </div>
-              //   </div>
-            )}
-            {/* {props.listing.type === "rent" && " / month"} */}
+          <p className="text-slate-500 mt-2 font-semibold">
+            &#8377;&nbsp;
+            {props.listing.offer
+              ? props.listing.offerPrice.toLocaleString("hi")
+              : props.listing.price.toLocaleString("hi")}
+            {props.listing.type === "rent" && " / month"}
           </p>
           <div className="text-slate-700 flex gap-4">
-            <div className="font-bold text-xs">
+            <div className="font-semi\bold text-xs">
               {props.listing.bedrooms > 1
                 ? `${props.listing.bedrooms} beds`
                 : `${props.listing.bedrooms} bed`}
             </div>
-            <div className="font-bold text-xs">
+            <div className="font-semi\bold text-xs">
               {props.listing.bathrooms > 1
                 ? `${props.listing.bathrooms} baths`
                 : `${props.listing.bathrooms} bed`}
