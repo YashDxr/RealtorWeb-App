b0523c88363509f300d4c910a2abf5f9b385076d
YashDxr <yashdxr@gmail.com>
2023-10-22 10:47:04 +0530

    SearchURL logic defined using URLSearchParams.get and .set methods

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,32 @@
 import { FaSearch, FaBuilding } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
-  const { currentUser } = useSelector(state => state.user);
-  const handleClick = () => {
-    console.log("user is",currentUser);
-  }
+  const [searchTerm, setSearchTerm] = useState("");
+  const { currentUser } = useSelector((state) => state.user);
+  // const handleClick = () => {
+  //   console.log("user is", currentUser);
+  // };
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTerm = urlParams.get("searchTerm");
+    if (searchTerm) {
+      setSearchTerm(searchTerm);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-indigo-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -17,13 +37,20 @@ export default function Header() {
             <span className="text-indigo-700">Web</span>
           </h1>
         </Link>
-        <form className="bg-indigo-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-indigo-100 p-3 rounded-lg flex items-center"
+        >
           <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-indigo-600" />
+          <button>
+            <FaSearch className="text-indigo-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
@@ -36,9 +63,14 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/profile" onClick={handleClick}>
+          <Link to="/profile">
             {currentUser ? (
-              <img className="rounded-full h-7 w-7" src={currentUser.avatar} alt="profile" referrerPolicy="no-referrer" />
+              <img
+                className="rounded-full h-7 w-7"
+                src={currentUser.avatar}
+                alt="profile"
+                referrerPolicy="no-referrer"
+              />
             ) : (
               <li className="sm:inline text-indigo-700 hover:underline cursor-pointer">
                 Sign In
